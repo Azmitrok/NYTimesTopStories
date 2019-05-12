902db18acbee67b4023585fd4434381688b29c2c
Azmitrok <azmitrok@gmail.com>
2019-05-12 20:28:06 +0300

    refactoring

--- NYTimesTopStoriesAPI/ApiException.cs
+++ NYTimesTopStoriesAPI/ApiException.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Runtime.Serialization;
 
-namespace NYTimesTopStoriesAPI.Services
+namespace NYTimesTopStoriesAPI
 {
     [Serializable]
     internal class ApiException : Exception
--- NYTimesTopStoriesAPI/NYTimesAPI/NYTimesApiProvider.cs
+++ NYTimesTopStoriesAPI/NYTimesAPI/NYTimesApiProvider.cs
@@ -7,7 +7,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace NYTimesTopStoriesAPI.NYTimesAPI
+namespace NYTimesTopStoriesAPI
 {
     public class NYTimesApiProvider
     {
