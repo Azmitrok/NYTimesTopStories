d0e947f0ed6301fb5a516710e7ac2b8973863678
Azmitrok <azmitrok@gmail.com>
2019-05-12 21:37:38 +0300

    Unit Tests

---
+++ NYTimesTopStoriesAPI.Tests/NYTimesApiUnitTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using NYTimesTopStoriesAPI.Models;
+using NYTimesTopStoriesAPI.Services;
+
+namespace NYTimesTopStoriesAPI.Tests
+{
+    [TestClass]
+    public class NYTimesApiUnitTest
+    {
+        private ISourceAPIService _service;
+
+        private JArray _sourceJArrayResults;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _service = new NYTimesAPIService(new TestApiProvider());
+
+            _sourceJArrayResults = (JArray)JObject.Parse(File.ReadAllText("Data/TestData.json"))["results"];
+        }
+
+
+        [TestMethod]
+        public void ApiStatusTest()
+        {
+            Assert.AreEqual("status:\"OK\"", _service.GetAPIStatus());
+        }
+
+        [TestMethod]
+        public void GetListBySectionTest()
+        {
+            IEnumerable<ArticleView> articles = _service.GetListBySectionAsync("home").Result;
+
+            List<ArticleView> articlesList = new List<ArticleView>();
+            foreach (var article in articles)
+                articlesList.Add(article);
+
+            Assert.AreEqual(_sourceJArrayResults.Count, articlesList.Count);
+        }
+
+        [TestMethod]
+        public void GetListBySectionFirstTest()
+        {
+            ArticleView article = _service.GetListBySectionFirstAsync("home").Result;
+
+            Assert.AreEqual(_sourceJArrayResults.First.Value<string>("title"), article.Heading);
+        }
+
+        [TestMethod]
+        public void GetListBySectionByUpdatedDateTest()
+        {
+            IEnumerable<ArticleView> articles = _service.GetListBySectionByUpdatedDateAsync("home", "2019-05-10").Result;
+
+            List<ArticleView> articlesList = new List<ArticleView>();
+            foreach (var article in articles)
+                articlesList.Add(article);
+
+            Assert.AreEqual(8, articlesList.Count);
+        }
+
+        [TestMethod]
+        public void GetArticleByShortUrlTest()
+        {
+            ArticleView article = _service.GetArticleByShortUrlAsync("2vU1134").Result;
+
+            Assert.AreEqual("Russia Is Targeting Europe\u2019s Elections. So Are Far-Right Copycats.", article.Heading);
+        }
+
+        [TestMethod]
+        public void GetGroupsBySectionTest()
+        {
+            IEnumerable<ArticleGroupByDateView> articlesGroups = _service.GetGroupsBySectionAsync("home").Result;
+
+            Assert.AreEqual(5, articlesGroups.Count());
+
+            Assert.AreEqual(8, articlesGroups.First(g => g.Date == "10.05.2019").Total);
+        }
+
+
+    }
+}
---
+++ NYTimesTopStoriesAPI.Tests/TestApiProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NYTimesTopStoriesAPI.Tests
+{
+    public class TestApiProvider : IApiProvider
+    {
+        public Task<string> GetArticlesBySectionContent(string section)
+        {
+            return Task.Run(() =>
+            {
+                return File.ReadAllText("Data/TestData.json");
+            });
+        }
+    }
+}
