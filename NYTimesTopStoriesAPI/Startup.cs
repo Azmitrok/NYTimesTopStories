2df4185405b9ec96b81d3f8076e21c5551c2d54c
Azmitrok <azmitrok@gmail.com>
2019-05-12 15:48:06 +0300

    Init

--- NYTimesTopStoriesAPI/Startup.cs
+++ NYTimesTopStoriesAPI/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Nancy.Owin;
 
 namespace NYTimesTopStoriesAPI
 {
@@ -20,6 +21,8 @@ namespace NYTimesTopStoriesAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseOwin(b => b.UseNancy());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
